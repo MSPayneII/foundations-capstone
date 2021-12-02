9de78dfc711356ca101f3183b73d326682acbb49
Michael Payne <mpaynei@umich.edu>
2021-12-01 20:00:01 -0500

    updating comments

--- server/controllers.js
+++ server/controllers.js
@@ -3,6 +3,7 @@ let fortunes = require("../db.json");
 let answerResponses = [];
 
 module.exports = {
+  // gets a randomized answer and send it to the client-side
   getRandomFortune: (req, res) => {
     // choose random fortune
     let randomIndex = Math.floor(Math.random() * fortunes.length);
@@ -11,10 +12,12 @@ module.exports = {
     res.status(200).send(randomFortune);
   },
 
+  // gets the answerResponses array of question/answer objects and send it to the client-side
   getAnswerResponses: (req, res) => {
     res.status(200).send(answerResponses);
   },
 
+  // creates a question/answer object, adds it to the answerResponses array, and sends it to the client-side
   createAnswerResp: (req, res) => {
     let { question, answer } = req.body;
 
@@ -27,6 +30,7 @@ module.exports = {
     res.status(200).send(answerResponses);
   },
 
+  // sets the answerResponses array to be an empty array
   deleteAnswerRespArr: (req, res) => {
     answerResponses = [];
     res.status(200).send(answerResponses);
